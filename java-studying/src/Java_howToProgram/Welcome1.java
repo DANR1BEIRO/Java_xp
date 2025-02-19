721791b81fa67140bbccb86d0e1c1c2038d72ad4
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-19 13:22:31 -0300

    Print something

---
+++ java-studying/src/Java_howToProgram/Welcome1.java
@@ -0,0 +1,16 @@
+package Java_howToProgram;
+// print text program
+
+public class Welcome1
+    {
+    // main method start a java application execution
+    public static void main(String[] args){
+        System.out.println("Welcome to Java Programming!");
+        System.out.print("\nWelcome to ");
+        System.out.println("Java Programming 2");
+        System.out.println("\nWelcome\nto\nJava\nprogramming 3");
+        System.out.printf("%n%s", "Welcome to Java programming 4 with format");
+
+    } // end of the main method
+} // end of the Welcome1 class
+
