326b725813baa8b9451bdae2bc4cd759471aad8e
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-20 12:23:46 -0300

    Addition

--- java-studying/src/Java_howToProgram/Addition.java
+++ java-studying/src/Java_howToProgram/Addition.java
@@ -7,9 +7,10 @@ public class Addition {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int number1;
-        int number2;
-        int sum;
+        int number1,
+            number2,
+            sum;
+//        int number1, number2, sum;
 
         System.out.print("Enter the first number: ");
         number1 = input.nextInt();
