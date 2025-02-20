df90378b15bf015f5871f177b70ca1aec0810479
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-20 10:13:31 -0300

    dio

---
+++ test/src/conditional.java
@@ -0,0 +1,11 @@
+public class conditional {
+
+    public static void main(String [] args) {
+        
+        int x = 5;
+        System.out.println("Good day");
+        if (x < 0) {
+            System.out.println("Good afternoon");
+        }
+    }
+}
