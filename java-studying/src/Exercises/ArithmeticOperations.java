ae28e57aebe6f3fb7694a38daa6116e5054fb723
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-20 13:05:10 -0300

    Take two integers and prints their sum, product, difference and quotient

---
+++ java-studying/src/Exercises/ArithmeticOperations.java
@@ -0,0 +1,33 @@
+package Exercises;
+
+/*
+Write an application that asks the user to enter two integers, obtains them
+from the user and prints their sum, product, difference and quotient (division)
+*/
+
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Enter the first integer: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter the second integer: ");
+        int b = input.nextInt();
+
+        System.out.println("The sum is: " + (a+b));
+        System.out.println("The product is: " + (a*b));
+        System.out.println("The difference is: " + (a-b));
+
+        if (b != 0)
+            System.out.println("The quotiente is: " + (a/b));
+        else
+            System.out.println("Error: Division by zero isn't allowed");
+
+        input.close();
+
+    }
+}
