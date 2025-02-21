5d537227d01a4c8f76e75f15c655c17ae836e636
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-21 13:33:41 -0300

    If statement

---
+++ java-studying/src/CubosAcademy/IfStatement.java
@@ -0,0 +1,18 @@
+package CubosAcademy;
+
+public class IfStatement {
+    public static void main(String[] args) {
+
+        int number1 = 12,
+            number2 = 15;
+
+        if (number1 < number2) {
+            System.out.println(number1 + " is less than " + number2);
+            System.out.println("without brackets");
+        } else if (number1 == number2) {
+            System.out.println(number1 + " is equal to " + number2);
+        } else {
+            System.out.println(number1 + " is greater than " + number2);
+        }
+    }
+}
